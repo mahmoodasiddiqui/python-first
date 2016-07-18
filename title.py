c3f8de3b8013bd5862d2a2daf9c3e8d2e70bf610
operation badar <ob@gmail.com>
2016-07-18 11:57:00 -0700

    title program

--- title.py
+++ title.py
@@ -1,2 +1,13 @@
 name = "mahmood ahmed siddiqui"
+print("Title with First Alphabet Capital")
 print(name.title())
+print("All Capital")
+print(name.upper())
+print("All Small")
+print(name.lower())
+
+fname = "mahmood"
+mname = "ahmed"
+lname = "siddiqui"
+print("First,Middle,Last Name Concatenation")
+print(fname+" "+mname+" "+lname)
