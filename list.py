4b9f3396e1d56e19a0e2a669de1664b80db49f21
operation badar <ob@gmail.com>
2016-07-19 10:35:54 -0700

    list program

---
+++ list.py
@@ -0,0 +1,10 @@
+name = ['mahmood', 'adnan', 'ayesha', 'rehan', 'asma','rana']
+print(name[0])
+print(name[0].title())
+print(name[-2])
+name[0] = 'mehmood'
+print(name[0])
+name.append('tauseef')
+print(name[-1])
+
+
