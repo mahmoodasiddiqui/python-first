839947030a7e61ee1f0ca566f7f8010b380d6be7
operation badar <ob@gmail.com>
2016-07-19 11:24:14 -0700

    Quiz1 second approach

--- quiz1.py
+++ quiz1.py
@@ -1,7 +1,10 @@
 count = 0
+c = []
 for i in range(2000, 3201):
     if (i%7==0) and (i%5!=0):
-        print(str(i)+',')
+        #print(str(i)+',')
+        c.append(i)
         count = count+1
-print(count)
-
+print("Total Numbers = "+str(count))
+for x in range (0, count):
+    print(c)
