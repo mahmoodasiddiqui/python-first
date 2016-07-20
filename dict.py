6d2be1f0c9ba72fa12b312fc7bc2284eb3d4f84b
operation badar <ob@gmail.com>
2016-07-20 12:47:26 -0700

    dictionary (years of exp)

---
+++ dict.py
@@ -0,0 +1,14 @@
+person_0={'name':'mahmood', 'office':'fft8','phone#':'03452107712'}
+print(person_0)
+person_0['degree']='ms'
+print(person_0)
+person_0['phone#']='03342107712'
+print(person_0)
+
+person_0['yoj']=2013
+person_0['cy']=2016
+
+exp = person_0['cy']- person_0['yoj']
+
+person_0['expr']= exp
+print(person_0)
