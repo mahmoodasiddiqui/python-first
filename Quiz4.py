3792ff44a41aff8b087e0c1b74615c8cb2899bf8
operation badar <ob@gmail.com>
2016-07-20 11:53:30 -0700

    Quiz 4 if-else (special character count)

--- Quiz4.py
+++ Quiz4.py
@@ -1,13 +1,17 @@
 msg = "hello world ! 123"
 digit =0
 letter =0
+special=0
 for i in msg:
     if i.isdigit():
         digit = digit+1
     elif i.isalpha():
         letter = letter+1
+    else:
+        special=special+1
 print("Digits :"+str(digit))
 print("Letter :"+str(letter))
+print("Special :"+str(special))
 
 
 
