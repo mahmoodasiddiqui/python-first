255dfd211ade7fe7e4e6e5f3353a14a9fa45037a
operation badar <ob@gmail.com>
2016-07-27 12:49:57 -0700

    min max mean

--- twod.py
+++ twod.py
@@ -7,6 +7,13 @@ print(m[:,2])
 print(m[:,1:3])
 n = m.ravel()
 print(n)
-
+print(m.transpose())
+print(m.max())
+print(m.min())
+print(m.mean())
+print(m.std())
+print(m.var())
+print(m.sum())
+print(m.prod())
 
 
